31fbeeca3959d18b70fc61234081b79a18cf23c5
Marco <marcosteam7@gmail.com>
2023-08-09 13:43:32 -0400

    move images

--- PixelMixer/PixelMixer/MainFrame.cpp
+++ PixelMixer/PixelMixer/MainFrame.cpp
@@ -1,6 +1,8 @@
 #include "MainFrame.h"
 #include <wx/wx.h>
 
+const std::string ICON_PATH = "C:/Users/marco/Documents/GitHub/PixelMixer/PixelMixer/Images/icon2.ico";
+
 enum IDs
 {
     exitB_ID = 2
@@ -8,11 +10,11 @@ enum IDs
 
 MainFrame::MainFrame(const wxString& title): wxFrame(nullptr, wxID_ANY, title) // constructor
 {
-    SetIcon(wxIcon("C:/Users/marco/Documents/GitHub/PixelMixer/Images/icon2.ico", wxBITMAP_TYPE_ICO)); // change to relative path
-    //SetWindowStyle(wxFRAME_NO_TASKBAR);
+    SetIcon(wxIcon(ICON_PATH, wxBITMAP_TYPE_ICO));
     wxWindowBase::SetBackgroundColour("#23272a"); // should this be in App.cpp
     
     SetClientSize(wxGetDisplaySize() * 0.8);
+    wxTopLevelWindowBase::SetMinSize(wxGetDisplaySize() * 0.3);
     Center();
 
     wxStatusBar* statusBar = wxFrameBase::CreateStatusBar();
