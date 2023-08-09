1fcf09b63476701303c9f4a3898e059ea27bc0a4
Marco <marcosteam7@gmail.com>
2023-08-09 11:33:02 -0400

    status bar and enum for IDs

--- PixelMixer/PixelMixer/MainFrame.cpp
+++ PixelMixer/PixelMixer/MainFrame.cpp
@@ -1,12 +1,19 @@
 #include "MainFrame.h"
 #include <wx/wx.h>
 
+enum IDs
+{
+    exitB_ID = 2
+};
+
 MainFrame::MainFrame(const wxString& title): wxFrame(nullptr, wxID_ANY, title) // constructor
 {
-    wxWindowBase::SetBackgroundColour("#23272a"); // should this be in App.cpp
     //SetWindowStyle(wxFRAME_NO_TASKBAR);
+    wxWindowBase::SetBackgroundColour("#23272a"); // should this be in App.cpp
     
     SetClientSize(wxGetDisplaySize() * 0.8);
     Center();
-    
+
+    wxStatusBar* statusBar = wxFrameBase::CreateStatusBar();
+    statusBar->SetBackgroundColour("#2c2f33");
 }
