f5f204cb765009ea49acc8a1a4ff8d96544e18ec
Marco <marcosteam7@gmail.com>
2023-08-10 13:34:52 -0400

    comments

--- PixelMixer/PixelMixer/MainFrame.cpp
+++ PixelMixer/PixelMixer/MainFrame.cpp
@@ -2,7 +2,11 @@
 #include <wx/wx.h>
 #include <wx/splitter.h>
 
-const std::string ICON_PATH = "C:/Users/marco/Documents/GitHub/PixelMixer/PixelMixer/Images/icon2.ico"; // not sure how to make relative yet
+//todo window size changes but the panel does not
+//todo make the vertical splitter sash a custom color
+//todo make the images paths relative
+
+const std::string ICON_PATH = "C:/Users/marco/Documents/GitHub/PixelMixer/PixelMixer/Images/icon2.ico";
 
 enum IDs
 {
@@ -11,32 +15,42 @@ enum IDs
 
 MainFrame::MainFrame(const wxString& title): wxFrame(nullptr, wxID_ANY, title, wxDefaultPosition, wxDefaultSize) // constructor
 {
+    // Layout setup
+
+    // Store the mem address of the created wxStatusBar object
     wxStatusBar* statusBar = wxFrameBase::CreateStatusBar();
 
-    auto* HorizontalSplitter = new wxSplitterWindow(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxSP_NOSASH);
-    auto* verticalSplitter = new wxSplitterWindow(HorizontalSplitter, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxSP_NOSASH);
-    
-    auto* configPanel = new wxPanel(verticalSplitter);
-    auto* viewportPanel = new wxPanel(verticalSplitter);
-    auto* customHeaderPanel = new wxPanel(HorizontalSplitter);
-    
-    configPanel->SetBackgroundColour("#282b30");
+    // Create the vertical and horizontal splitters to organize the frame
+    auto* hSplitter = new wxSplitterWindow(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxSP_NOSASH);
+    auto* vSplitter = new wxSplitterWindow(hSplitter, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxSP_NOSASH);
+
+    // Create the panels to color the split regions
+    auto* configPanel = new wxPanel(vSplitter);
+    auto* viewportPanel = new wxPanel(vSplitter);
+    auto* headerPanel = new wxPanel(hSplitter);
+
+    // Set the panel colors
     viewportPanel->SetBackgroundColour("#36393e");
-    customHeaderPanel->SetBackgroundColour("#2c2f33");
+      configPanel->SetBackgroundColour("#282b30");
+      headerPanel->SetBackgroundColour("#2c2f33");
 
-    verticalSplitter->SetMinimumPaneSize(200);
-    verticalSplitter->SetSashGravity(0);
-    verticalSplitter->SplitVertically(configPanel, viewportPanel);
-    verticalSplitter->SetSashPosition(100);
+    // Configure the vertical splitter
+        vSplitter->SetMinimumPaneSize(200);
+        vSplitter->SetSashGravity(0);
+        vSplitter->SplitVertically(configPanel, viewportPanel);
+        vSplitter->SetSashPosition(100);
 
-    HorizontalSplitter->SplitHorizontally(customHeaderPanel, verticalSplitter);
-    HorizontalSplitter->SetSashPosition(30);
+    // configure the horizontal splitter
+        hSplitter->SplitHorizontally(headerPanel, vSplitter);
+        hSplitter->SetSashPosition(30);
 
+    // Configure the main window
     wxTopLevelWindowBase::SetMinSize(wxGetDisplaySize() * 0.3);
                statusBar->SetBackgroundColour("#2c2f33");
                           SetWindowStyle(wxFRAME_NO_TASKBAR);
                           SetIcon(wxIcon(ICON_PATH, wxBITMAP_TYPE_ICO));
                           SetClientSize(wxGetDisplaySize() * 0.8);
     CenterOnScreen();
+    // Layout setup end
 }
 
