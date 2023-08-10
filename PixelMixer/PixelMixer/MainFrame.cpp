770f1f2f319d779f4366cf336baadfd09064bca7
Marco <marcosteam7@gmail.com>
2023-08-10 16:01:28 -0400

    Draggable frame and resize fix

--- PixelMixer/PixelMixer/MainFrame.cpp
+++ PixelMixer/PixelMixer/MainFrame.cpp
@@ -2,23 +2,25 @@
 #include <wx/wx.h>
 #include <wx/splitter.h>
 
-//todo window size changes but the panel does not
 //todo make the vertical splitter sash a custom color
-//todo make the images paths relative
-
-const std::string ICON_PATH = "C:/Users/marco/Documents/GitHub/PixelMixer/PixelMixer/Images/icon2.ico";
 
 enum IDs
 {
-    exitB_ID = 2
+    header_ID = 2
 };
 
 MainFrame::MainFrame(const wxString& title): wxFrame(nullptr, wxID_ANY, title, wxDefaultPosition, wxDefaultSize) // constructor
 {
     // Layout setup
-
+    
     // Store the mem address of the created wxStatusBar object
     wxStatusBar* statusBar = wxFrameBase::CreateStatusBar();
+
+    // Configure the main window
+    statusBar->SetBackgroundColour("#2c2f33");
+    SetClientSize(wxGetDisplaySize() * 0.8);
+    SetIcon(wxIcon(icon_path_, wxBITMAP_TYPE_ICO));
+    wxTopLevelWindowBase::SetMinSize(wxSize((GetSize().GetWidth() / 5) * 2, (GetSize().GetHeight() / 5) * 2)); // Min size is double the config column
 
     // Create the vertical and horizontal splitters to organize the frame
     auto* hSplitter = new wxSplitterWindow(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxSP_NOSASH);
@@ -27,7 +29,7 @@ MainFrame::MainFrame(const wxString& title): wxFrame(nullptr, wxID_ANY, title, w
     // Create the panels to color the split regions
     auto* configPanel = new wxPanel(vSplitter);
     auto* viewportPanel = new wxPanel(vSplitter);
-    auto* headerPanel = new wxPanel(hSplitter);
+    auto* headerPanel = new wxPanel(hSplitter, header_ID);
 
     // Set the panel colors
     viewportPanel->SetBackgroundColour("#36393e");
@@ -35,22 +37,50 @@ MainFrame::MainFrame(const wxString& title): wxFrame(nullptr, wxID_ANY, title, w
       headerPanel->SetBackgroundColour("#2c2f33");
 
     // Configure the vertical splitter
-        vSplitter->SetMinimumPaneSize(200);
-        vSplitter->SetSashGravity(0);
-        vSplitter->SplitVertically(configPanel, viewportPanel);
-        vSplitter->SetSashPosition(100);
+        vSplitter->SplitVertically(configPanel, viewportPanel); // Split the left(config) and right(viewport)
+        vSplitter->SetSashPosition(GetSize().GetWidth() / 5);
 
     // configure the horizontal splitter
-        hSplitter->SplitHorizontally(headerPanel, vSplitter);
-        hSplitter->SetSashPosition(30);
-
-    // Configure the main window
-    wxTopLevelWindowBase::SetMinSize(wxGetDisplaySize() * 0.3);
-               statusBar->SetBackgroundColour("#2c2f33");
-                          SetWindowStyle(wxFRAME_NO_TASKBAR);
-                          SetIcon(wxIcon(ICON_PATH, wxBITMAP_TYPE_ICO));
-                          SetClientSize(wxGetDisplaySize() * 0.8);
+        hSplitter->SplitHorizontally(headerPanel, vSplitter); // Split the top(header) and bottom(main region)
+        hSplitter->SetSashPosition(FromDIP(30)); // is DIP necessary?
+    
+    SetWindowStyle(wxSYSTEM_MENU | wxRESIZE_BORDER| wxCLIP_CHILDREN); // Must be after status bar declaration
     CenterOnScreen();
     // Layout setup end
+
+    //Bind header to event handlers so the window can be dragged and maximized
+    headerPanel->Bind(wxEVT_LEFT_DOWN, &MainFrame::OnHeaderLeftDown, this, header_ID);
+    headerPanel->Bind(wxEVT_LEFT_UP, &MainFrame::OnHeaderLeftUp, this, header_ID);
+    headerPanel->Bind(wxEVT_MOTION, &MainFrame::OnMouseMove, this, header_ID);
+    headerPanel->Bind(wxEVT_LEFT_DCLICK, &MainFrame::OnTitleBarDoubleClick, this, header_ID);
+    
+}
+
+void MainFrame::OnHeaderLeftDown(const wxMouseEvent& e)
+{
+    isDragging_ = true;
+    dragStart_ = e.GetPosition();
+}
+
+void MainFrame::OnHeaderLeftUp(wxMouseEvent& e)
+{
+    isDragging_ = false;
+}
+
+void MainFrame::OnMouseMove(wxMouseEvent& e)
+{
+    if (isDragging_) {
+        const wxPoint newPos = e.GetPosition() - dragStart_;
+        SetPosition(GetPosition() + newPos);
+    }
+}
+
+void MainFrame::OnTitleBarDoubleClick(wxMouseEvent& e)
+{
+    if (IsMaximized()) {
+        Restore();
+    } else {
+        Maximize();
+    }
 }
 
--- PixelMixer/PixelMixer/MainFrame.h
+++ PixelMixer/PixelMixer/MainFrame.h
@@ -5,4 +5,53 @@ class MainFrame final : public wxFrame
 {
 public:
     explicit MainFrame(const wxString& title);
+private:
+    const std::string icon_path_ = "C:/Users/marco/Documents/GitHub/PixelMixer/PixelMixer/Images/icon2.ico";
+    
+    bool isDragging_;
+    wxPoint dragStart_;
+    void OnHeaderLeftDown(const wxMouseEvent& e);
+    void OnHeaderLeftUp(wxMouseEvent& e);
+    void OnMouseMove(wxMouseEvent& e);
+    void OnTitleBarDoubleClick(wxMouseEvent& e);
+
+
+    // https://stackoverflow.com/questions/41179437/wxwidgets-wxborder-none-and-wxresize-border-makes-white-area
+    WXLRESULT MSWWindowProc(WXUINT nMsg, WXWPARAM wParam, WXLPARAM lParam) override
+    {
+        switch (nMsg)
+        {
+        case WM_NCACTIVATE:
+            {
+                lParam = -1;
+                break;
+            }
+        case WM_NCCALCSIZE:
+            if (wParam)
+            {
+                const HWND hWnd = (HWND)this->GetHandle();
+                WINDOWPLACEMENT wPos;
+                wPos.length = sizeof(wPos);
+                GetWindowPlacement(hWnd, &wPos);
+                if (wPos.showCmd != SW_SHOWMAXIMIZED)
+                {
+                    RECT borderThickness;
+                    SetRectEmpty(&borderThickness);
+                    AdjustWindowRectEx(&borderThickness,
+                        GetWindowLongPtr(hWnd, GWL_STYLE) & ~WS_CAPTION, FALSE, NULL);
+                    borderThickness.left *= -1;
+                    borderThickness.top *= -1;
+                    NCCALCSIZE_PARAMS* sz = reinterpret_cast<NCCALCSIZE_PARAMS*>(lParam);
+                    sz->rgrc[0].top += 1;
+                    sz->rgrc[0].left += borderThickness.left;
+                    sz->rgrc[0].right -= borderThickness.right;
+                    sz->rgrc[0].bottom -= borderThickness.bottom;
+                    return 0;
+                }
+            }
+            break;
+        default: ;
+        }
+        return wxFrame::MSWWindowProc(nMsg, wParam, lParam);
+    }
 };
