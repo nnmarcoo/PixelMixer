f4187557a3f53ba67355538dbfcd593e742b95db
Marco <marcosteam7@gmail.com>
2023-08-15 11:37:53 -0400

    repaint fix

--- PixelMixer/PixelMixer/ViewportPanel.cpp
+++ PixelMixer/PixelMixer/ViewportPanel.cpp
@@ -1,19 +1,23 @@
 #include "ViewportPanel.h"
 #include "FileDropTarget.h"
 
+wxBEGIN_EVENT_TABLE(ViewportPanel, wxPanel)
+    EVT_SIZE(ViewportPanel::OnSize)
+    EVT_PAINT(ViewportPanel::OnPaint)
+    EVT_DROP_FILES(ViewportPanel::OnDropFiles)
+wxEND_EVENT_TABLE()
+
 ViewportPanel::ViewportPanel(wxWindow* parent) : wxPanel(parent) {
+    canDND = true;
     wxWindowBase::SetBackgroundColour("#36393e");
 
-    // Enable drag-and-drop support
+    // Enable DND
     if (canDND)
         wxWindow::SetDropTarget(new FileDropTarget(this));
-    Bind(wxEVT_DROP_FILES, &ViewportPanel::OnDropFiles, this);
 }
 
 void ViewportPanel::OnDropFiles(wxDropFilesEvent& e) {
     LoadAndDisplayImage(e.GetFiles()[0]);
-    canDND = false;
-    SetDropTarget(nullptr);
     e.Skip();
 }
 
@@ -22,18 +26,29 @@ void ViewportPanel::LoadAndDisplayImage(const wxString& filePath) {
     const wxImage image(filePath, wxBITMAP_TYPE_ANY);
 
     if (image.IsOk()) {
-        // Calculate the center position
-        int centerX = (GetSize().GetWidth() - image.GetWidth()) / 2;
-        int centerY = (GetSize().GetHeight() - image.GetHeight()) / 2;
-        
-        // Create a bitmap from the image
-        wxBitmap bitmap(image);
-        
-        // Create a memory DC and draw the image onto it
-        wxClientDC dc(this);
-        wxMemoryDC memDC;
-        memDC.SelectObject(bitmap);
-        dc.Blit(centerX, centerY, image.GetWidth(), image.GetHeight(), &memDC, 0, 0);
+        // Store image data and center positions
+        loadedBitmap = wxBitmap(image);
+        imageCenterX = (GetSize().GetWidth() - image.GetWidth()) / 2;
+        imageCenterY = (GetSize().GetHeight() - image.GetHeight()) / 2;
+
+        Refresh();  // Trigger repainting
         canDND = false;
     }
+}
+
+void ViewportPanel::OnPaint(wxPaintEvent& event) {
+    wxPaintDC dc(this);
+
+    if (loadedBitmap.IsOk()) {
+        dc.DrawBitmap(loadedBitmap, imageCenterX, imageCenterY, true);
+    }
+}
+
+void ViewportPanel::OnSize(wxSizeEvent& event) {
+    // Update center position based on the new panel size
+    imageCenterX = (GetSize().GetWidth() - loadedBitmap.GetWidth()) / 2;
+    imageCenterY = (GetSize().GetHeight() - loadedBitmap.GetHeight()) / 2;
+
+    Refresh();  // Trigger repainting
+    event.Skip();
 }
--- PixelMixer/PixelMixer/ViewportPanel.h
+++ PixelMixer/PixelMixer/ViewportPanel.h
@@ -7,9 +7,16 @@ public:
     void LoadAndDisplayImage(const wxString& filePath);
 
 private:
+    wxDECLARE_EVENT_TABLE();
+    
     wxString droppedFilePath;
     bool canDND = true;
+    wxBitmap loadedBitmap;
+    int imageCenterX;
+    int imageCenterY;
     
     void OnDropFiles(wxDropFilesEvent& event);
+    void OnPaint(wxPaintEvent& event);
+    void OnSize(wxSizeEvent& event);
     
 };
