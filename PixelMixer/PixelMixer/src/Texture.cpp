02c68cba945fbd04bb1e72bd3cfd82e078152a13
Marco <marcosteam7@gmail.com>
2023-12-05 02:20:01 -0500

    Unnecessary include removed

--- PixelMixer/PixelMixer/src/Texture.cpp
+++ PixelMixer/PixelMixer/src/Texture.cpp
@@ -1,4 +1,5 @@
 #include "Texture.h"
+#include "Renderer.h"
 #include "vendor/stb_image/stb_image.h"
 
 Texture::Texture(const std::string& path) : RendererID_(0), FilePath_(path), LocalBuffer_(nullptr), Width_(0), Height_(0), BPP_(0){
