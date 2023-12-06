09219272a3529572ff9de76b808b4bebd9b7f684
Marco <marcosteam7@gmail.com>
2023-12-06 01:31:47 -0500

    Modified to use Texture object

--- PixelMixer/PixelMixer/src/FrameBuffer.cpp
+++ PixelMixer/PixelMixer/src/FrameBuffer.cpp
@@ -1,18 +1,19 @@
 #include "FrameBuffer.h"
 #include <iostream>
 #include "Renderer.h"
+#include "Texture.h"
 
 FrameBuffer::FrameBuffer(unsigned width, unsigned height) : RendererID_(0), TextureID_(0), RenderBufferID_(0), Width_(width), Height_(height), ClearColor_(1) {
+    // Create the texture internally in the constructor
+    const Texture tempTexture(width, height);
+    TextureID_ = tempTexture.GetRendererID();
+    Width_ = tempTexture.GetWidth();
+    Height_ = tempTexture.GetHeight();
+
     GLCall(glGenFramebuffers(1, &RendererID_))
     GLCall(glBindFramebuffer(GL_FRAMEBUFFER, RendererID_))
     
-    GLCall(glGenTextures(1, &TextureID_)) // TODO: use texture object
-    GLCall(glBindTexture(GL_TEXTURE_2D, TextureID_))
-    GLCall(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, Width_, Height_, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr))
-    GLCall(glBindTexture(GL_TEXTURE_2D, 0))
     GLCall(glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, TextureID_, 0))
-    GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE))
-    GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE))
     
     GLCall(glGenRenderbuffers(1, &RenderBufferID_))
     GLCall(glBindRenderbuffer(GL_RENDERBUFFER, RenderBufferID_))
